1fb8e1c14f267e321e5f7db11b149104f9901dad
Microindole <1513979779@qq.com>
2025-04-19 11:07:27 +0800

    用栈实现以算符优先法实现算术表达式

---
+++ Stack/Stack.cpp
@@ -0,0 +1,14 @@
+﻿// Stack.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Stack.h"
+//#include "ArrayStack.h"
+using namespace std;
+
+int main(){
+	string input;
+	getline(cin, input);
+	get_result(input);
+
+}
